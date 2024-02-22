d40abcbe991e3dfd4d42092a93588e03d52910c5
Konrad Kubczyk <konrad@kubczyk.com>
2024-02-22 23:48:54 +0100

    feat: add React Native Paper

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {MD3Theme as DefaultTheme, PaperProvider} from "react-native-paper";
+
+const theme = {
+    ...DefaultTheme
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider theme={theme}>
+            <View style={styles.container}>
+                <Text
+                    style={{fontSize: 32}}
+                >
+                    Hello, world!
+                </Text>
+                <StatusBar style="auto"/>
+            </View>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
